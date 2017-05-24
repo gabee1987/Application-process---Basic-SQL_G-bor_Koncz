9b011d157306e2da6b8331ef9a1a3df4bca8c63e
gabee1987 <gabee1987@gmail.com>
2017-05-24 14:36:52 +0200

    Created all-school.html and all_school function with query

--- part2/main.py
+++ part2/main.py
@@ -1,3 +1,9 @@
+'''
+    Application process - SQL part 2 assaignment
+    main module
+    by Gabor Koncz
+'''
+
 from flask import Flask, request, render_template, redirect
 import psycopg2
 from database_handler import query_manager
@@ -26,14 +32,27 @@ def mentors():
     table_headers = [
                     'Mentor name',
                     'School name',
-                    'Country',
+                    'Country'
                     ]
     query = mentors_and_shools_query
     mentors_data = query_manager(query, 'all_data')
     return render_template('mentors.html', table_headers=table_headers, mentors_data=mentors_data)
 
 
-
+@app.route('/all-school')
+def all_school():
+    '''
+        Displays the /all-school page.
+        Shows the data as a table.
+    '''
+    table_headers = [
+                    'Mentor name',
+                    'School name',
+                    'Country'
+                    ]
+    query = all_school_query
+    all_school_data = query_manager(query, 'all_data')
+    return render_template('all-school.html', table_headers=table_headers, all_school_data=all_school_data)
 
 
 if __name__ == '__main__':
--- part2/queries.py
+++ part2/queries.py
@@ -1,3 +1,9 @@
+'''
+    Application process - SQL part 2 assaignment
+    queries module
+    by Gabor Koncz
+'''
+
 import psycopg2
 
 mentors_and_shools_query = """SELECT mentors.first_name || ' ' || mentors.last_name AS mentor_name, schools.name, schools.country\
@@ -7,4 +13,8 @@ mentors_and_shools_query = """SELECT mentors.first_name || ' ' || mentors.last_n
                                 ORDER BY mentors.id;"""
 
 
-#all_schools_query = 
+all_school_query = """SELECT mentors.first_name || ' ' || mentors.last_name AS mentor_name, schools.name, schools.country\
+                        FROM mentors\
+                        RIGHT OUTER JOIN schools\
+                        ON mentors.city=schools.city\
+                        ORDER BY mentors.id;"""
